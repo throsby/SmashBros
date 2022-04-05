dcc4d89d179e9a2c3d88e2669bc745a2f7be01fc
Throsby Wells <throsbywells@gmail.com>
2022-04-05 13:14:58 -0400

    created fighter-screen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
         <Fighter name={"Roy"} color={"red"} />
         <Fighter name={"Fox"} color={"green"} />
         <Fighter name={"Ganondorf"} color={"purple"} />
+        <Fighter name={"Sheik"} color={"blue"} />
+      </div>
+      <div className='fighter-screen'>
+        <h2>Roy</h2>
+        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png"/>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie a iaculis at erat pellentesque adipiscing commodo. Tortor consequat id porta nibh venenatis cras sed felis. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Aliquet bibendum enim facilisis gravida neque convallis a cras. Magna sit amet purus gravida quis. At tellus at urna condimentum mattis. Integer vitae justo eget magna fermentum iaculis. Urna porttitor rhoncus dolor purus non. Nec ultrices dui sapien eget.
+        </p>
       </div>
     </div>
   );
